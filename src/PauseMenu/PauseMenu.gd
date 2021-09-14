2819a6402c543eaf6336c7facc14347dd255ea31
Oliver Theobald <Oliver_Theobald@web.de>
2021-09-14 14:21:05 +0200

    feat(PauseMenu)Add PauseMenu

--- src/PauseMenu/PauseMenu.gd
+++ src/PauseMenu/PauseMenu.gd
@@ -5,8 +5,7 @@ export(String, FILE) var first_scene
 
 # Path to the Selector's
 onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector_1
-onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector_2
-onready var selector_three = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector_3
+onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer3/VBoxContainer/CenterContainer/HBoxContainer/Selector_2
 
 var current_selection = 0
 
@@ -15,7 +14,7 @@ func _ready():
 
 # Choose option
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_down") and current_selection < 2:
+	if Input.is_action_just_pressed("ui_down") and current_selection < 1:
 		current_selection += 1
 		set_current_selection(current_selection)
 	elif Input.is_action_just_pressed("ui_up") and current_selection > 0:
@@ -28,18 +27,14 @@ func handle_selection(_current_selection):
 	if _current_selection == 0:
 		SceneLoader.change_scene(first_scene, 'fade')
 	elif _current_selection == 1:
-		print("ToDo")
-	elif _current_selection == 2:
 		get_tree().quit()
 
 #Set Arrow on selected option
 func set_current_selection(_current_selection):
 	selector_one.text = ""
 	selector_two.text = ""
-	selector_three.text = ""
 	if _current_selection == 0:
 		selector_one.text = ">"
 	elif _current_selection == 1:
 		selector_two.text = ">"
-	elif _current_selection == 2:
-		selector_three.text = ">"
+	
